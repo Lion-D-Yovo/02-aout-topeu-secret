be4030c5709117569536615a14f81bd2bbc61245
Lion-D-Yovo <lion.abonnements@gmail.com>
2018-08-02 20:55:43 +0200

    push to heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'users#index'
+  root to: 'static_pages#home'
+  resources :users
+
+  get '/answer', to:'static_pages#answer', as: 'answer'
+
 end
